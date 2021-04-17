5fe8da2e78f2f46ee4bf1c56c31de1229b73a1fc
tolumide-ng <tolumideshopein@gmail.com>
2021-04-17 10:30:12 +0100

    adds support for upload image on the browse button

--- src/main/components/UI/organisms/DropFile/index.tsx
+++ src/main/components/UI/organisms/DropFile/index.tsx
@@ -1,24 +1,65 @@
 import * as React from "react";
+import { confirmSize } from "../../../../utilities/helpers/files";
+import { Button } from "../../atoms/Button";
 import { NeuMorphLogo } from "../../atoms/NeumorphLogo";
 import styles from "./index.module.css";
 
 export const DropFile = () => {
+    const fileElem = React.useRef<HTMLInputElement>(null);
+    const fileSelect = React.useRef<HTMLButtonElement>(null);
+
+    const [theFile, setTheFile] = React.useState<File | undefined>(undefined);
+    const [fileError, setFileError] = React.useState("");
+
+    const handleAcceptUpload = (e: React.ChangeEvent) => {
+        const target = e.target as HTMLInputElement;
+        const file: File = (target.files as FileList)[0];
+
+        if (file) {
+            setFileError("");
+            try {
+                confirmSize(file.size);
+                setTheFile(file);
+            } catch (error) {
+                console.log("the receoved error", error);
+                setFileError(error);
+            }
+        }
+    };
+
+    const handleBrowseImage = (e: React.TouchEvent | React.MouseEvent) => {
+        if (fileElem.current) {
+            fileElem.current.click();
+        }
+    };
+
     return (
         <article className={styles.drpFile}>
             <form encType="multipart/form-data" className={styles.drpFileForm}>
                 <div className={styles.drpFileDiv}>
                     <NeuMorphLogo />
-                    <input
-                        type="file"
-                        id="fileElem"
-                        multiple
-                        accept=".shapr"
-                    ></input>
 
-                    <p className={styles.drpFileText}>
+                    <div className={styles.drpFileText}>
+                        {/* <label htmlFor="fileElem">
+                            Drop your .shapr file here, or
+                        </label> */}
                         Drop your .shapr file here, or{" "}
-                        <span className={styles.drpFileBrowse}>browse</span>
-                    </p>
+                        <input
+                            type="file"
+                            id="fileElem"
+                            multiple
+                            accept=".shapr"
+                            ref={fileElem}
+                            className={styles.drpFileInput}
+                            onChange={handleAcceptUpload}
+                        ></input>
+                        <Button
+                            handleClick={handleBrowseImage}
+                            buttonText="browse"
+                            buttonClass={styles.drpFileBrowse}
+                            ref={fileSelect}
+                        />
+                    </div>
                     <p>
                         <small className={styles.drpComment}>
                             Supports: .STEP, .STL and .IGES
