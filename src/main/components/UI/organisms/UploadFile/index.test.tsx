f0a626ada3b6b016dadeb0c9cc00893488f3ae19
tolumide-ng <tolumideshopein@gmail.com>
2021-04-18 00:37:15 +0100

    adds test for shadow, upload, and converttemplate component

---
+++ src/main/components/UI/organisms/UploadFile/index.test.tsx
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom";
+import * as React from "react";
+import { render } from "@testing-library/react";
+import { UploadFile } from ".";
+
+describe("UploadFile Component", () => {
+    test("Mounts upload file container component", async () => {
+        const FILE_NAME = "test_file.shapr";
+
+        const { getByRole } = render(<UploadFile fileName={FILE_NAME} />);
+
+        const element = getByRole("article");
+
+        expect(element).toBeTruthy();
+        expect(element).toHaveTextContent(FILE_NAME);
+        expect(element).toHaveTextContent("Uploading...");
+    });
+});
